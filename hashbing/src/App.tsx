af82b8a3cceaeb43e3c512c8b1d8fc8d4c6ee897
Leo Jacob <33173777+leojacob7@users.noreply.github.com>
2022-04-06 22:26:40 -0400

    tailwind, firebase fix, docker-compose update

--- hashbing/src/App.tsx
+++ hashbing/src/App.tsx
@@ -1,29 +1,29 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 // import "./App.css";
-import axios, { AxiosResponse } from "axios";
-import Login from "./Components/Login";
-import LoginTutor from "./Components/LoginTutor";
-import Signup from "./Components/Signup";
-import "./App.css";
-import Courses from "./Components/Courses";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import axios, { AxiosResponse } from 'axios'
+import Login from './Components/Login'
+import LoginTutor from './Components/LoginTutor'
+import Signup from './Components/Signup'
+import './App.css'
+import Courses from './Components/Courses'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState<string | AxiosResponse<string, any>>("");
+function App () {
+  const [count, setCount] = useState(0)
+  const [data, setData] = useState({})
 
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className='App'>
         <Routes>
-          <Route path="/studentlogin" element={<Login />}></Route>
-          <Route path="/tutorlogin" element={<LoginTutor />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/courses" element={<Courses />}></Route>
+          <Route path='/studentlogin' element={<Login />}></Route>
+          <Route path='/tutorlogin' element={<LoginTutor />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+          <Route path='/courses' element={<Courses />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
