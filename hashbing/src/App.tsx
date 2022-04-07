8061be3bc6acb20061b6ff89e94d8913038f61dd
govindr15 <govindr15895@gmail.com>
2022-04-06 21:41:17 -0400

    Adding ui related files for login,signup and course listing page

--- hashbing/src/App.tsx
+++ hashbing/src/App.tsx
@@ -1,25 +1,29 @@
-import { useState, useEffect } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import axios, { AxiosResponse } from 'axios'
+import { useState, useEffect } from "react";
+// import "./App.css";
+import axios, { AxiosResponse } from "axios";
+import Login from "./Components/Login";
+import LoginTutor from "./Components/LoginTutor";
+import Signup from "./Components/Signup";
+import "./App.css";
+import Courses from "./Components/Courses";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App () {
-  const [count, setCount] = useState(0)
-  const [data, setData] = useState<string | AxiosResponse<string, any>>('')
+function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState<string | AxiosResponse<string, any>>("");
 
-  useEffect(() => {
-    axios
-      .post('http://localhost:4000/users/signup', {
-        username: 'testuser1',
-        password: 'testuser',
-        email: 'testuser1@test.com'
-      })
-      .then(res => {
-        setData(res.data)
-      })
-  }, [])
-
-  return <div className='App'>{JSON.stringify(data)}</div>
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/studentlogin" element={<Login />}></Route>
+          <Route path="/tutorlogin" element={<LoginTutor />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/courses" element={<Courses />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
