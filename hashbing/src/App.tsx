77595331de3f107a707ba4b9f2ad2b0da74737eb
govindr15 <govindr15895@gmail.com>
2022-04-28 12:18:41 -0400

    Adding course details, enrolled pages and front end validations for signup and login pages

--- hashbing/src/App.tsx
+++ hashbing/src/App.tsx
@@ -1,29 +1,33 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 // import "./App.css";
-import axios, { AxiosResponse } from 'axios'
-import Login from './Components/Login'
-import LoginTutor from './Components/LoginTutor'
-import Signup from './Components/Signup'
-import './App.css'
-import Courses from './Components/Courses'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import axios, { AxiosResponse } from "axios";
+import Login from "./Components/Login";
+import LoginTutor from "./Components/LoginTutor";
+import Signup from "./Components/Signup";
+import "./App.css";
+import Courses from "./Components/Courses";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CourseDetails from "./Components/CourseDetails";
+import Enrolled from "./Components/Enrolled";
 
-function App () {
-  const [count, setCount] = useState(0)
-  const [data, setData] = useState({})
+function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState({});
 
   return (
     <BrowserRouter>
-      <div className='App'>
+      <div className="App">
         <Routes>
-          <Route path='/studentlogin' element={<Login />}></Route>
-          <Route path='/tutorlogin' element={<LoginTutor />}></Route>
-          <Route path='/signup' element={<Signup />}></Route>
-          <Route path='/courses' element={<Courses />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/tutorlogin" element={<LoginTutor />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/" element={<Courses />}></Route>
+          <Route path="/courses/:id" element={<CourseDetails />}></Route>
+          <Route path="/enrolled" element={<Enrolled />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- hashbing/src/Components/Header.tsx
+++ hashbing/src/Components/Header.tsx
@@ -1,15 +1,31 @@
 import React from "react";
-
+interface items {
+  section: string;
+}
 function Header() {
+  // if (props.section === "courses") {
+  //   const courses = document.getElementById("courses");
+  // } else if (props.details === "enrolled") {
+  //   const enrolled = document.getElementById("enrolled");
+  // } else if (props.details === "user") {
+  //   const user = document.getElementById("user");
+  // }
   return (
-    <div className="flex w-full shadow-2xl shadow-black">
+    <div className="flex w-full shadow-2xl">
       <div className="flex w-1/2 justify-start"></div>
       <div className="flex w-1/2 justify-end">
-        <div className="text-md font-bold p-5 text-blue-400 cursor-pointer">
+        <div
+          id="courses"
+          className="text-md font-bold p-5 text-blue-400 cursor-pointer"
+        >
           Courses
         </div>
-        <div className="text-md font-bold p-5 cursor-pointer">Enrolled</div>
-        <div className="text-md font-bold p-5 cursor-pointer">User</div>
+        <div id="enrolled" className="text-md font-bold p-5 cursor-pointer">
+          Enrolled
+        </div>
+        <div id="user" className="text-md font-bold p-5 cursor-pointer">
+          User
+        </div>
         <div className="text-md font-bold p-5 cursor-pointer">Logout</div>
       </div>
     </div>
