3eda575d2a68c415e387e3c5207e7d75a67b7650
govindr15 <govindr15895@gmail.com>
2022-04-30 18:59:27 -0400

    Few UI related changes

--- hashbing/src/App.tsx
+++ hashbing/src/App.tsx
@@ -1,20 +1,19 @@
 import { useState, useEffect } from "react";
 // import "./App.css";
-import axios, { AxiosResponse } from 'axios'
-import Login from './Components/Login'
-import LoginTutor from './Components/LoginTutor'
-import Signup from './Components/Signup'
-import './App.css'
-import Courses from './Components/Courses'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import CourseDetails from './Components/CourseDetails'
-import Enrolled from './Components/Enrolled'
-import Header from './Components/Header'
-import Users from './Components/Users'
-import Authored from './Components/Authored'
-import NewCourseContainer from './courseCreation/NewCourseContainer'
-import NewCourseWizard from './courseCreation/NewCourseWizard'
-
+import axios, { AxiosResponse } from "axios";
+import Login from "./Components/Login";
+import LoginTutor from "./Components/LoginTutor";
+import Signup from "./Components/Signup";
+import "./App.css";
+import Courses from "./Components/Courses";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CourseDetails from "./Components/CourseDetails";
+import Enrolled from "./Components/Enrolled";
+import Header from "./Components/Header";
+import Users from "./Components/Users";
+import Authored from "./Components/Authored";
+import NewCourseContainer from "./courseCreation/NewCourseContainer";
+import NewCourseWizard from "./courseCreation/NewCourseWizard";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -24,30 +23,28 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/tutorlogin' element={<LoginTutor />}></Route>
-          <Route path='/signup' element={<Signup />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/tutorlogin" element={<LoginTutor />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
         </Routes>
-        <div className='flex min-h-screen min-w-screen bg-sky-400'>
-          <div className='flex bg-white m-20 w-screen rounded-2xl'>
-            <div className='flex flex-col w-full h-full rounded-2xl'>
-              <Header />
-              <Routes>
-                <Route path='/' element={<Courses />}></Route>
-                <Route path='/courses/:id' element={<CourseDetails />}></Route>
-                <Route path='/enrolled' element={<Enrolled />}></Route>
-                <Route path='/authored' element={<Authored />}></Route>
-                <Route path='/users' element={<Users />}></Route>
-                <Route
-                  path='/courses/new'
-                  element={<NewCourseContainer />}
-                ></Route>
-                <Route
-                  path='/course/new/:id'
-                  element={<NewCourseWizard />}
-                ></Route>
-              </Routes>
-            </div>
+        <div className="flex bg-white w-screen rounded-2xl">
+          <div className="flex flex-col w-full h-full rounded-2xl">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Courses />}></Route>
+              <Route path="/courses/:id" element={<CourseDetails />}></Route>
+              <Route path="/enrolled" element={<Enrolled />}></Route>
+              <Route path="/authored" element={<Authored />}></Route>
+              <Route path="/users" element={<Users />}></Route>
+              <Route
+                path="/courses/new"
+                element={<NewCourseContainer />}
+              ></Route>
+              <Route
+                path="/course/new/:id"
+                element={<NewCourseWizard />}
+              ></Route>
+            </Routes>
           </div>
         </div>
       </div>
