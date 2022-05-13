dc8728f3fc7a22d5efc8712c00e273c80f76465d
Leo B Jacob <leobjacob@gmail.com>
2022-05-12 23:41:32 -0400

    add bearer token

--- hashbing/src/Components/CourseDetails.tsx
+++ hashbing/src/Components/CourseDetails.tsx
@@ -1,83 +1,83 @@
-import React, { useEffect, useState } from "react";
-import Header from "./Header";
-import logo from "../Images/course1.png";
+import React, { useEffect, useState } from 'react'
+import Header from './Header'
+import logo from '../Images/course1.png'
 import {
   faCircleCheck,
   faClipboardCheck,
-  faXmarkSquare,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useParams } from "react-router-dom";
-import { createToken } from "../firebase";
-import { faXingSquare } from "@fortawesome/free-brands-svg-icons";
+  faXmarkSquare
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useParams } from 'react-router-dom'
+import { createToken } from '../firebase'
+import { faXingSquare } from '@fortawesome/free-brands-svg-icons'
 
 type courseDetailType = {
-  title: string;
-  description: string;
-  courseOutcome: { [key: string]: string };
-  author: string;
-  topicsTagged: [string];
-};
+  title: string
+  description: string
+  courseOutcome: { [key: string]: string }
+  author: string
+  topicsTagged: [string]
+}
 
-function CourseDetails() {
-  const [course, setCourse] = useState<courseDetailType>();
-  const params = useParams();
-  const [rerender, setRerender] = useState(false);
-  const [enrolled, setEnrolled] = useState<String[]>([]);
-  const cid = params.id;
+function CourseDetails () {
+  const [course, setCourse] = useState<courseDetailType>()
+  const params = useParams()
+  const [rerender, setRerender] = useState(false)
+  const [enrolled, setEnrolled] = useState<String[]>([])
+  const cid = params.id
 
   useEffect(() => {
     const fetchData = async () => {
-      const header = await createToken();
-      const url = `http://localhost:4000/courses/${cid}/`;
+      const header = await createToken()
+      const url = `http://localhost:4000/courses/${cid}/`
       fetch(url, {
-        method: "GET",
-        headers: header.headers,
+        method: 'GET',
+        headers: header.headers
       })
-        .then(async (response) => {
-          const cou = await response.json();
-          console.log(cou);
-          setCourse(cou);
+        .then(async response => {
+          const cou = await response.json()
+          console.log(cou)
+          setCourse(cou)
         })
-        .catch((error) => console.log(error.message));
-    };
-    fetchData();
+        .catch(error => console.log(error.message))
+    }
+    fetchData()
     return () => {
-      fetchData;
-    };
-  }, [rerender]);
+      fetchData
+    }
+  }, [rerender])
 
   useEffect(() => {
     const fetchEnrolled = async () => {
-      const url = `http://localhost:4000/courses/Enrolled/`;
+      const url = `http://localhost:4000/courses/Enrolled/`
       // const requestOptions = {
       //   method: "GET",
       // };
-      const header = await createToken();
+      const header = await createToken()
       fetch(url, {
-        method: "GET",
+        method: 'GET',
         headers: header.headers,
-        credentials: "include",
+        credentials: 'include'
       })
-        .then(async (response) => {
-          const cou = await response.json();
-          console.log(cou);
-          setEnrolled(cou.Enrolled);
+        .then(async response => {
+          const cou = await response.json()
+          console.log(cou)
+          setEnrolled(cou.Enrolled)
         })
-        .catch((error) => console.log(error.message));
-    };
-    fetchEnrolled();
+        .catch(error => console.log(error.message))
+    }
+    fetchEnrolled()
     return () => {
-      fetchEnrolled;
-    };
-  }, [rerender]);
+      fetchEnrolled
+    }
+  }, [rerender])
   const enrollCourse = async (
     e: React.MouseEvent<HTMLButtonElement>,
     id: string
   ) => {
-    e.preventDefault();
-    const header = await createToken();
-    const url = `http://localhost:4000/courses/${id}/enroll`;
+    e.preventDefault()
+    const header = await createToken()
+    const url = `http://localhost:4000/courses/${id}/enroll`
     // const requestOptions = {
     //   method: "PUT",
     //   headers: { "Content-Type": "application/json" },
@@ -85,23 +85,23 @@ function CourseDetails() {
     //   credentials: "same-origin",
     // };
     fetch(url, {
-      method: "PUT",
+      method: 'PUT',
       headers: header.headers,
-      credentials: "include",
+      credentials: 'include'
     })
-      .then((response) => {
-        console.log("Status Changed Successfully");
-        setRerender(!rerender);
+      .then(response => {
+        console.log('Status Changed Successfully')
+        setRerender(!rerender)
       })
-      .catch((error) => console.log(error.message));
-  };
+      .catch(error => console.log(error.message))
+  }
 
   const unregisterCourse = async (
     e: React.MouseEvent<HTMLButtonElement>,
     id: string
   ) => {
-    const header = await createToken();
-    const url = `http://localhost:4000/courses/${id}/unregister`;
+    const header = await createToken()
+    const url = `http://localhost:4000/courses/${id}/unregister`
     // const requestOptions = {
     //   method: "PUT",
     //   headers: { "Content-Type": "application/json" },
@@ -109,53 +109,53 @@ function CourseDetails() {
     //   credentials: "same-origin",
     // };
     fetch(url, {
-      method: "PUT",
+      method: 'PUT',
       headers: header.headers,
-      credentials: "include",
+      credentials: 'include'
     })
-      .then((response) => {
-        console.log("Status Changed Successfully");
-        setRerender(!rerender);
+      .then(response => {
+        console.log('Status Changed Successfully')
+        setRerender(!rerender)
       })
-      .catch((error) => console.log(error.message));
-  };
+      .catch(error => console.log(error.message))
+  }
 
   return (
-    <div className="flex bg-white w-screen rounded-2xl">
-      <div className="flex flex-col w-full h-full rounded-2xl">
+    <div className='flex bg-white w-screen rounded-2xl'>
+      <div className='flex flex-col w-full h-full rounded-2xl'>
         {/* <Header selection='courses' /> */}
-        <div className="flex bg-gray-200 h-full rounded-b-2xl">
-          <div className="flex flex-col basis-3/4 h-full">
-            <div className="flex-col w-full h-full">
-              <div className="flex m-10 p-6 align-middle items-center rounded-2xl h-full bg-sky-400 shadow-2xl">
-                <div className="flex-col">
+        <div className='flex bg-gray-200 h-full rounded-b-2xl'>
+          <div className='flex flex-col basis-3/4 h-full'>
+            <div className='flex-col w-full h-full'>
+              <div className='flex m-10 p-6 align-middle items-center rounded-2xl h-full bg-sky-400 shadow-2xl'>
+                <div className='flex-col'>
                   <br />
-                  <p className="text-4xl text-white font-sans font-bold text-left pl-10">
+                  <p className='text-4xl text-white font-sans font-bold text-left pl-10'>
                     {course?.title}
                   </p>
                   <br />
-                  <p className="text-md text-white font-sans font-bold text-left pl-10">
+                  <p className='text-md text-white font-sans font-bold text-left pl-10'>
                     {course?.description}
                   </p>
                   <br />
-                  <p className="text-sm text-white font-sans font-bold pl-10">
+                  <p className='text-sm text-white font-sans font-bold pl-10'>
                     Created by :
-                    <span className="text-xs font-sans font-semibold pl-2 text-black">
+                    <span className='text-xs font-sans font-semibold pl-2 text-black'>
                       {course?.author}
                     </span>
                   </p>
                   <br />
-                  <p className="text-sm text-white font-sans font-bold pl-10">
+                  <p className='text-sm text-white font-sans font-bold pl-10'>
                     Last Updated on :
-                    <span className="text-xs font-sans font-semibold pl-2 text-black">
+                    <span className='text-xs font-sans font-semibold pl-2 text-black'>
                       15/05/2021
                     </span>
                   </p>
                   {/* Tags */}
-                  <div className="flex-grow mt-4 ml-5 mr-5">
-                    {course?.topicsTagged?.map((tag) => (
+                  <div className='flex-grow mt-4 ml-5 mr-5'>
+                    {course?.topicsTagged?.map(tag => (
                       <span
-                        className="text-xs font-semibold text-center py-1 px-2 rounded text-cyan-600 bg-blue-200 uppercase m-4"
+                        className='text-xs font-semibold text-center py-1 px-2 rounded text-cyan-600 bg-blue-200 uppercase m-4'
                         key={tag}
                       >
                         {tag}
@@ -165,18 +165,18 @@ function CourseDetails() {
                 </div>
               </div>
             </div>
-            <div className="flex-col w-full mb-20 mt-10 h-full">
-              <div className="flex rounded-xl align-middle items-center m-10 h-full bg-white shadow-2xl">
-                <div className="flex-col p-10">
-                  <p className="text-3xl font-sans font-bold text-left pl-10 pb-10">
+            <div className='flex-col w-full mb-20 mt-10 h-full'>
+              <div className='flex rounded-xl align-middle items-center m-10 h-full bg-white shadow-2xl'>
+                <div className='flex-col p-10'>
+                  <p className='text-3xl font-sans font-bold text-left pl-10 pb-10'>
                     What You will Learn
                   </p>
                   <br />
-                  <div className="flex flex-wrap">
+                  <div className='flex flex-wrap'>
                     {course &&
-                      Object.keys(course.courseOutcome)?.map((id) => (
-                        <h1 className="text-xs font-sans text-left pl-10 w-1/2">
-                          <FontAwesomeIcon icon={faCircleCheck} size={"1x"} />{" "}
+                      Object.keys(course.courseOutcome)?.map(id => (
+                        <h1 className='text-xs font-sans text-left pl-10 w-1/2'>
+                          <FontAwesomeIcon icon={faCircleCheck} size={'1x'} />{' '}
                           {course?.courseOutcome[id]}
                         </h1>
                       ))}
@@ -222,27 +222,27 @@ function CourseDetails() {
               </div>
             </div> */}
           </div>
-          <div className="flex flex-col basis-1/5 rounded-br-2xl h-full relative">
-            <div className="flex-col h-10"></div>
-            <div className="flex cursor-pointer h-96 fixed">
-              <div className="flex-col h-full mr-20 mt-10 bg-white shadow-2xl">
+          <div className='flex flex-col basis-1/5 rounded-br-2xl h-full relative'>
+            <div className='flex-col h-10'></div>
+            <div className='flex cursor-pointer h-96 fixed'>
+              <div className='flex-col h-full mr-20 mt-10 bg-white shadow-2xl'>
                 {/* Img */}
-                <div className="flex justify-center">
-                  <img src={logo} alt="" className="h-40 w-full object-fill" />
+                <div className='flex justify-center'>
+                  <img src={logo} alt='' className='h-40 w-full object-fill' />
                 </div>
                 {/* <div className="flex w-full border-b-2 border-gray-400"></div> */}
                 {/* Topic */}
-                <div className="flex-col">
-                  <div className="flex-col min-h-16">
-                    <p className="text-lg font-sans font-bold text-center pl-2">
+                <div className='flex-col'>
+                  <div className='flex-col min-h-16'>
+                    <p className='text-lg font-sans font-bold text-center pl-2'>
                       {course?.title}
                     </p>
                   </div>
                   {/* Tags */}
-                  <div className="flex-grow ml-4">
-                    {course?.topicsTagged?.map((tag) => (
+                  <div className='flex-grow ml-4'>
+                    {course?.topicsTagged?.map(tag => (
                       <span
-                        className="text-xs font-semibold text-center py-1 px-2 rounded text-cyan-600 bg-blue-200 uppercase m-4"
+                        className='text-xs font-semibold text-center py-1 px-2 rounded text-cyan-600 bg-blue-200 uppercase m-4'
                         key={tag}
                       >
                         {tag}
@@ -250,40 +250,40 @@ function CourseDetails() {
                     ))}
                   </div>
                   {/* Details Section */}
-                  <div className="flex-col mt-4 bg-white">
-                    <div className="flex justify-center">
+                  <div className='flex-col mt-4 bg-white'>
+                    <div className='flex justify-center'>
                       {cid != undefined && enrolled?.includes(cid) ? (
                         <button
-                          className="bg-sky-400 p-3 w-4/5 mb-4 text-white"
-                          onClick={(e) => unregisterCourse(e, cid)}
+                          className='bg-sky-400 p-3 w-4/5 mb-4 text-white'
+                          onClick={e => unregisterCourse(e, cid)}
                         >
                           Unregister &nbsp;
                           <FontAwesomeIcon
                             icon={faXmarkSquare}
-                            className="relative"
-                            color={"white"}
-                            size={"1x"}
+                            className='relative'
+                            color={'white'}
+                            size={'1x'}
                           />
                         </button>
                       ) : (
                         cid != undefined && (
                           <button
-                            className="bg-sky-400 p-3 w-4/5 mb-4 text-white"
-                            onClick={(e) => enrollCourse(e, cid)}
+                            className='bg-sky-400 p-3 w-4/5 mb-4 text-white'
+                            onClick={e => enrollCourse(e, cid)}
                           >
                             Enroll &nbsp;
                             <FontAwesomeIcon
                               icon={faClipboardCheck}
-                              className="relative"
-                              color={"white"}
-                              size={"1x"}
+                              className='relative'
+                              color={'white'}
+                              size={'1x'}
                             />
                           </button>
                         )
                       )}
                     </div>
-                    <div className="flex justify-center">
-                      <button className="text-sky-400 p-3">
+                    <div className='flex justify-center'>
+                      <button className='text-sky-400 p-3'>
                         Start Learning
                       </button>
                     </div>
@@ -292,11 +292,11 @@ function CourseDetails() {
               </div>
             </div>
           </div>
-          <div className="basis-1/12 h-full"></div>
+          <div className='basis-1/12 h-full'></div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default CourseDetails;
+export default CourseDetails
