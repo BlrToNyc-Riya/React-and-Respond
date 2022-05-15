e5bba9c91c4bd73efc8e139ccdaddaba4ffda74c
Leo B Jacob <leobjacob@gmail.com>
2022-05-15 19:19:47 -0400

    status codes

--- hashbing-server/data/courses.js
+++ hashbing-server/data/courses.js
@@ -43,9 +43,9 @@ module.exports = {
 		isValidObject(courseOutcome);
 		const usersCollection = await users();
 		const user = await usersCollection.findOne({ email: author });
-		if (user === null) throw 'No user with that id';
+		if (user === null) throw { message: 'No user with that id', code: 404 };
 		console.log(user);
-		if (!user) throw 'Author not found';
+		if (!user) throw { message: 'Author not found', code: 404 };
 
 		const courseCollection = await courses();
 		const course = {
@@ -61,7 +61,8 @@ module.exports = {
 		};
 
 		const newCourseInfo = await courseCollection.insertOne(course);
-		if (newCourseInfo.insertedCount === 0) throw 'Insert failed!';
+		if (newCourseInfo.insertedCount === 0)
+			throw { message: 'Insert failed!', code: 500 };
 		console.log(newCourseInfo.insertedId);
 		user.courseAuthored.push(objectIdToString(newCourseInfo.insertedId));
 		const userCollection = await users();
@@ -70,25 +71,28 @@ module.exports = {
 			{ $set: user }
 		);
 		if (!updateInfo.matchedCount && !updateInfo.modifiedCount)
-			throw 'Update failed';
+			throw { message: 'Update failed', code: 500 };
 		console.log(user);
 		return 'Insertion successful';
 	},
 	async getCourseById(id) {
-		if (!id || typeof id != 'string') throw 'Invalid id passed';
+		if (!id || typeof id != 'string')
+			throw { message: 'Invalid id passed', code: 400 };
 		const courseId = ObjectID(id);
 		const courseCollection = await courses();
 		const course = await courseCollection.findOne({ _id: courseId });
-		if (!course) throw 'No course found with this id';
+		if (!course)
+			throw { message: 'No course found with this id', code: 404 };
 		return course;
 	},
 	async getCourseByIdPerUser(id, email) {
-		if (!id || typeof id != 'string') throw 'Invalid id passed';
+		if (!id || typeof id != 'string')
+			throw { message: 'Invalid id passed', code: 404 };
 		const courseId = ObjectID(id);
 		const usersCollection = await users();
 		const user = await usersCollection.findOne({ email });
 		console.log('user', user.coursesEnrolled, id);
-		if (!user) throw { message: 'User not found', status: 404 };
+		if (!user) throw { message: 'User not found', code: 404 };
 		const isUserEnrolled = user.coursesEnrolled.filter((courseId) => {
 			console.log(id);
 			return id.toString() === courseId.toString();
@@ -97,31 +101,44 @@ module.exports = {
 		if (isUserEnrolled.length <= 0)
 			throw {
 				message: 'User is not enrolled to this course/ Access Denied',
-				status: 403,
+				code: 403,
 			};
 		const courseCollection = await courses();
 		const course = await courseCollection.findOne({ _id: courseId });
-		if (!course) throw 'No course found with this id';
+		if (!course)
+			throw { message: 'No course found with this id', code: 404 };
 		return course;
 	},
 
 	async enrollToCourse(email, courseId) {
 		if (!email || !courseId)
-			throw "Error with user's email or the course id not fetched..";
+			throw {
+				message:
+					"Error with user's email or the course id not fetched..",
+				code: 400,
+			};
 		courseId = ObjectID(courseId);
 		const courseCollection = await courses();
 		const course = await courseCollection.findOne({ _id: courseId });
-		if (!course) throw 'No course found with this id, unable to register';
+		if (!course)
+			throw {
+				message: 'No course found with this id, unable to register',
+				code: 404,
+			};
 		const userCollection = await users();
 		const user = await userCollection.findOne({ email });
 		if (!user) {
-			throw 'Invalid user login, unable to register for the course';
+			throw {
+				message:
+					'Invalid user login, unable to register for the course',
+				code: 404,
+			};
 		}
 		const found = user.coursesEnrolled.find(
 			(element) => element == objectIdToString(courseId)
 		);
 		if (found) {
-			throw 'Course already enrolled';
+			throw { message: 'Course already enrolled', code: 403 };
 		}
 		user.coursesEnrolled.push(objectIdToString(courseId));
 		const updateInfo = await userCollection.updateOne(
@@ -129,7 +146,7 @@ module.exports = {
 			{ $set: user }
 		);
 		if (!updateInfo.matchedCount && !updateInfo.modifiedCount)
-			throw 'Update failed';
+			throw { message: 'Update failed', code: 500 };
 		return 'Enrolled successfully!!';
 	},
 	async unregisterCourse(email, courseId) {
@@ -142,7 +159,11 @@ module.exports = {
 		const userCollection = await users();
 		const user = await userCollection.findOne({ email });
 		if (!user) {
-			throw 'Invalid user login, unable to register for the course';
+			throw {
+				message:
+					'Invalid user login, unable to register for the course',
+				code: 403,
+			};
 		}
 		user.coursesEnrolled = user.coursesEnrolled.filter(function (
 			value,
@@ -156,45 +177,69 @@ module.exports = {
 			{ $set: user }
 		);
 		if (!updateInfo.matchedCount && !updateInfo.modifiedCount)
-			throw 'Update failed';
+			throw { message: 'Update failed', code: 500 };
 		return 'Unregistered successfully!!';
 	},
 	async getAllAuthoredCourses(email) {
-		if (!email) throw 'You must be signed in to update profile..';
+		if (!email)
+			throw {
+				message: 'You must be signed in to update profile..',
+				code: 403,
+			};
 		const userCollection = await users();
 		const user = await userCollection.findOne({ email });
-		if (!user) throw 'Could not fetch user data';
+		if (!user) throw { message: 'Could not fetch user data', code: 404 };
 		return user.courseAuthored;
 	},
 	async getAllEnrolledCourses(email) {
-		if (!email) throw 'You must be signed in to update profile..';
+		if (!email)
+			throw {
+				message: 'You must be signed in to update profile..',
+				code: 403,
+			};
 		const userCollection = await users();
 		const user = await userCollection.findOne({ email });
-		if (!user) throw 'Could not fetch user data';
+		if (!user) throw { message: 'Could not fetch user data', code: 404 };
 		return user.coursesEnrolled;
 	},
 	async getAllCourses() {
 		const courseCollection = await courses();
 		const allCourses = await courseCollection.find({}).toArray();
-		if (!allCourses) throw 'could not fetch courses';
+		if (!allCourses)
+			throw { message: 'could not fetch courses', error: 401 };
 		return allCourses;
 	},
 
 	async deleteCourse(email, courseId) {
 		if (!email || !courseId)
-			throw "Error with user's email or the course id not fetched..";
+			throw {
+				message:
+					"Error with user's email or the course id not fetched..",
+				code: 403,
+			};
 		parsedId = ObjectID(courseId);
 		const courseCollection = await courses();
 		const course = await courseCollection.findOne({ _id: parsedId });
-		if (!course) throw 'No course found with this id, unable to unregister';
+		if (!course)
+			throw {
+				message: 'No course found with this id, unable to unregister',
+				code: 404,
+			};
 		const userCollection = await users();
 		const user = await userCollection.findOne({ email });
 		if (!user) {
-			throw 'User not found, unable to delete the course';
+			throw {
+				message: 'User not found, unable to delete the course',
+				code: 404,
+			};
 		}
 		const authoredCourse = user.courseAuthored;
 		if (!authoredCourse.includes(courseId)) {
-			throw 'This course is not created by the logged in user. Only the created user can delete the course!';
+			throw {
+				message:
+					'This course is not created by the logged in user. Only the created user can delete the course!',
+				code: 403,
+			};
 		}
 		const usersCollection = await users();
 		const updateInfo = await usersCollection.updateMany(
@@ -208,11 +253,12 @@ module.exports = {
 		);
 		console.log(updateInfo);
 		if (!updateInfo.matchedCount && !updateInfo.modifiedCount)
-			throw 'Update failed';
+			throw { message: 'Update failed', code: 500 };
 		let userList = await userCollection.find({}).toArray();
 		console.log(userList);
 		const deleteInfo = await courseCollection.deleteOne({ _id: parsedId });
-		if (deleteInfo.deletedCount === 0) throw 'delete failed';
+		if (deleteInfo.deletedCount === 0)
+			throw { message: 'delete failed', code: 500 };
 		return 'deleted successfully!!';
 	},
 };
